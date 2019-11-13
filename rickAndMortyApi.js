6d645f6f329e81240253934486aaff421728db4c
irojas <irojas@arsat.com.ar>
2019-11-13 15:32:32 -0300

    se agrega mas carga

--- rickAndMortyApi.js
+++ rickAndMortyApi.js
@@ -1,38 +1,41 @@
-var numberPage = 1;
-function nextPage() {
-    console.log(numberPage);
-return numberPage++;
+let numberPage = 1;
+let limit = 5;
+window.onload = function () { 
+    nextPage();
 }
- // Replace ./data.json with your JSON feed
-fetch(`https://rickandmortyapi.com/api/character?page=${numberPage}`) 
-
-    .then(response => {
-       return response.json();
-
-    })
-    .then(data => {
-        // Work with JSON data here 
-        getImgSrc(data);
-    })
-    .catch(err => {
-        // Do something for an error here
-        console.log('hubo un error ' , err);
-    })
-    //********************************************************************************/ 
+//********************************************************************************/ 
+function nextPage() {
+     // Replace ./data.json with your JSON feed
+        fetch(`https://rickandmortyapi.com/api/character/?page=${numberPage}&limit=${limit}`) 
+        .then(response => {
+        return response.json();
+        })
+        .then(data => {
+            // Work with JSON data here 
+           
+            getImgSrc(data);
+        })
+        .catch(err => {
+            // Do something for an error here
+            console.log('hubo un error ' , err);
+        })
+    return numberPage++;
+}
+//********************************************************************************/ 
 function getImgSrc(data) {
-   
+    console.log(data);
     const caja = document.getElementById('root');
     imgSrcs = data.results;
-    
     imgSrcs.forEach(imgSrc => {          
         const img = document.createElement('img');
         const h2 = document.createElement('h2');
         const card = document.createElement('div');
-        card.setAttribute('class', 'card');
-        caja.appendChild(card);
-        card.appendChild(img);
-        card.appendChild(h2);
-        img.src = imgSrc.image;
-        h2.textContent = imgSrc.name;
+            card.setAttribute('class', 'card');
+            card.setAttribute('id', 'card');
+            caja.appendChild(card);
+            card.appendChild(img);
+            card.appendChild(h2);
+            img.src = imgSrc.image;
+            h2.textContent = imgSrc.name;
     });
 }
